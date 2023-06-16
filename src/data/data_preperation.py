0f3a29ec948297642e232ff1e4802c23c543531d
Rhys Jones <rhys.jones@e2s.energy>
2023-06-16 14:48:20 +0100

    Task: added FillMissingData & GenerateDatetime

--- src/data/data_preperation.py
+++ src/data/data_preperation.py
@@ -30,17 +30,19 @@ class OutlierRemover(DataPreparationProtocol):
         else:
             raise ValueError("Unsupported data type. Please provide a NumPy array or DataFrame.")
         outliers = self.find_outliers(values)
-        if fill_na:
-            if is_dataframe:
-                return self.fill_drop_na(data=data, outliers=outliers, func='fillna')
-            else:
-                values[np.isnan(values)] = np.mean(values)
-                return values
-        else:
-            if is_dataframe:
-                return self.fill_drop_na(data=data, outliers=outliers, func='dropna')
-            else:
-                return values[~np.any(outliers, axis=1)]
+        data[outliers] = np.nan
+        return data
+        # if fill_na:
+        #     if is_dataframe:
+        #         return self.fill_drop_na(data=data, outliers=outliers, func='fillna')
+        #     else:
+        #         values[np.isnan(values)] = np.mean(values)
+        #         return values
+        # else:
+        #     if is_dataframe:
+        #         return self.fill_drop_na(data=data, outliers=outliers, func='dropna')
+        #     else:
+        #         return values[~np.any(outliers, axis=1)]
             
     def find_outliers(self, values: np.ndarray) -> np.ndarray:
         """
@@ -57,26 +59,72 @@ class OutlierRemover(DataPreparationProtocol):
             outliers[:, column_idx] = (column_data < lower_bound) | (column_data > upper_bound)
         return outliers
 
-    def fill_drop_na(self, data: np.ndarray, outliers: np.ndarray, func: str) -> np.ndarray:
+    # def fill_drop_na(self, data: np.ndarray, outliers: np.ndarray, func: str) -> np.ndarray:
+    #     if func == 'dropna':
+    #         data[outliers] = np.nan
+    #         data = data.dropna()
+    #         return data
+    #     elif func == 'fillna':
+    #         data[outliers] = np.nan
+    #         data = data.fillna(data.mean())
+    #         return data
+
+
+class FillMissingData(DataPreparationProtocol):
+    """
+    Fills missing values in array/dataframe.
+    """
+    def data_modifier(self, data: np.ndarray | pd.DataFrame, func: str) -> np.ndarray | pd.DataFrame:
+        """
+        Fill missing values in the data.
+        """
         if func == 'dropna':
-            data[outliers] = np.nan
-            data = data.dropna()
-            return data
+            data = self.dropna(data)
         elif func == 'fillna':
-            data[outliers] = np.nan
-            data = data.fillna(data.mean())
+            data = self.fillna(data)
+        else:
+            raise ValueError("Invalid fill method. Please choose 'dropna' or 'fillna'.")
+        
+        return data
+    
+    def dropna(self, data: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+        """
+        Drop rows with missing values.
+        """
+        if isinstance(data, pd.DataFrame):
+            return data.dropna()
+        elif isinstance(data, np.ndarray):
+            return data[~np.isnan(data).any(axis=1)]
+        else:
+            raise ValueError("Unsupported data type. Please provide a NumPy array or DataFrame.")
+    
+    def fillna(self, data: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+        """
+        Fill missing values with column means.
+        """
+        if isinstance(data, pd.DataFrame):
+            return data.fillna(data.mean())
+        elif isinstance(data, np.ndarray):
+            data[np.isnan(data)] = np.nanmean(data)
             return data
-
-
+        else:
+            raise ValueError("Unsupported data type. Please provide a NumPy array or DataFrame.")
+    
 class GenerateDatetime(DataPreparationProtocol):
     """
     Creates a datetime for dataset or without one.
     """
-    def data_modifier(self,
-                     data: np.ndarray | pd.DataFrame | None,
-                     start_date: datetime,
-                     freq: str) -> np.ndarray | pd.DataFrame:
+    def data_modifier(self, start_date: datetime,
+                      data: np.ndarray | pd.DataFrame | None = None,
+                      freq: str = '30T',
+                      length: int = 48) -> np.ndarray | pd.DataFrame:
         """
         Create datetime for a dataset or independent of one.
         """
-
+        if data is None:
+            num_steps = length
+        else:
+            num_steps = len(data)
+        
+        datetime_array = pd.date_range(start=start_date, periods=num_steps, freq=freq)
+        return datetime_array
