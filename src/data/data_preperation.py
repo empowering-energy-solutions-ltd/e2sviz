1f7b791401f59c81df591176712b15533535f236
Rhys Jones <rhys.jones@e2s.energy>
2023-06-16 15:32:57 +0100

    Task: clearing pylint warnings

--- src/data/data_preperation.py
+++ src/data/data_preperation.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 
 class DataPreparationProtocol:
-  def data_modifier(self, 
-                    data: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+  def data_modifier(self,
+                    data: np.ndarray |
+                    pd.DataFrame) -> np.ndarray | pd.DataFrame:
     """
     Cleans the data for manipulation and visualisation.
     """
@@ -15,7 +16,8 @@ class DataPreparationProtocol:
 class OutlierRemover(DataPreparationProtocol):
   """ Removes outliers from array/dataframe """ 
   def data_modifier(self,
-                    data: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+                    data: np.ndarray |
+                    pd.DataFrame) -> np.ndarray | pd.DataFrame:
     """
     Remove outliers from the data.
     Parameters:
@@ -28,11 +30,13 @@ class OutlierRemover(DataPreparationProtocol):
     elif isinstance(data, np.ndarray):
       values = data
     else:
-      raise ValueError("Unsupported data type. Please provide a NumPy array or DataFrame.")
+      raise ValueError(
+        "Unsupported data type. Please provide a NumPy array or DataFrame."
+        )
     outliers = self.find_outliers(values)
     data[outliers] = np.nan
     return data
-      
+
   def find_outliers(self, values: np.ndarray) -> np.ndarray:
     """
     Find outliers in the data.
@@ -45,7 +49,8 @@ class OutlierRemover(DataPreparationProtocol):
       iqr = q3 - q1
       lower_bound = q1 - (1.5 * iqr)
       upper_bound = q3 + (1.5 * iqr)
-      outliers[:, column_idx] = (column_data < lower_bound) | (column_data > upper_bound)
+      outliers[:, column_idx] = (
+        column_data < lower_bound)|(column_data > upper_bound)
     return outliers
 
 
@@ -54,13 +59,16 @@ class FillMissingData(DataPreparationProtocol):
   """
   Fills missing values in array/dataframe.
   """
-  def data_modifier(self, data: np.ndarray | pd.DataFrame, func: str) -> np.ndarray | pd.DataFrame:
+  def data_modifier(self,
+                    data: np.ndarray | pd.DataFrame,
+                    func: str) -> np.ndarray | pd.DataFrame:
     """
     Fill missing values in the data.
 
     Parameters:
       Data: Either numpy array or pandas dataframe
-      Func: 'fillna' or 'dropna' at the moment fill only does mean but could be expanded.
+      Func: 'fillna' or 'dropna' at the moment fill only does mean 
+      but could be expanded.
     Returns:
       Array or Dataframe, whichever you gave it in the first place.
     """
@@ -69,33 +77,44 @@ class FillMissingData(DataPreparationProtocol):
     elif func == "fillna":
       data = self.fillna(data)
     else:
-      raise ValueError("Invalid fill method. Please choose 'dropna' or 'fillna'.")
-    
+      raise ValueError(
+        "Invalid fill method. Please choose 'dropna' or 'fillna'."
+        )
+
     return data
-    
-  def dropna(self, data: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+
+  def dropna(self,
+             data: np.ndarray |
+             pd.DataFrame) -> np.ndarray | pd.DataFrame:
     if isinstance(data, pd.DataFrame):
       return data.dropna()
     elif isinstance(data, np.ndarray):
       return data[~np.isnan(data).any(axis=1)]
     else:
-      raise ValueError("Unsupported data type. Please provide a NumPy array or DataFrame.")
-    
-  def fillna(self, data: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+      raise ValueError(
+        "Unsupported data type. Please provide a NumPy array or DataFrame."
+        )
+
+  def fillna(self,
+             data: np.ndarray |
+             pd.DataFrame) -> np.ndarray | pd.DataFrame:
     if isinstance(data, pd.DataFrame):
       return data.fillna(data.mean())
     elif isinstance(data, np.ndarray):
       data[np.isnan(data)] = np.nanmean(data)
       return data
     else:
-      raise ValueError("Unsupported data type. Please provide a NumPy array or DataFrame.")
-    
+      raise ValueError(
+        "Unsupported data type. Please provide a NumPy array or DataFrame."
+        )
+
 class GenerateDatetime(DataPreparationProtocol):
   """
   Creates a datetime for dataset or without one.
   """
-  def data_modifier(self, start_date: datetime,
+  def data_modifier(self,
                     data: np.ndarray | pd.DataFrame | None = None,
+                    start_date: datetime = datetime(2022,1,1),
                     freq: str = "30T",
                     periods: int = 48) -> np.ndarray | pd.DataFrame:
     """
@@ -112,5 +131,7 @@ class GenerateDatetime(DataPreparationProtocol):
       num_steps = periods
     else:
       num_steps = len(data)
-    datetime_array = pd.date_range(start=start_date, periods=num_steps, freq=freq)
+    datetime_array = pd.date_range(start=start_date,
+                                   periods=num_steps,
+                                   freq=freq)
     return datetime_array
